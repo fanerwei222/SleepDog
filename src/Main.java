21a27e6c224c46d4bb3af23c25383121d225a737
fanwei <fanerwei222@163.com>
2017-11-08 16:32:46 +0800

    main更新.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Pattern pattern = Pattern.compile("href=\"(.+?)\"");
+        Matcher matcher = pattern.matcher("＜a href=\"index.html\"＞我的主页＜/a＞");
+        if (matcher.find()){
+            System.out.println(matcher.group(1));
+        }
     }
 }
